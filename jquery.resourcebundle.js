791fe95e7f942938347f9ca4d5a0356f37cba334
acatl pacheco <acatl.pacheco@gmail.com>
2011-04-07 16:55:50 -0500

    first commit

---
+++ jquery.resourcebundle.js
@@ -0,0 +1,40 @@
+/*
+ * ResourceBundle
+ *
+ * Copyright 2010, Acatl Pacheco
+ * Licensed under the MIT License.
+ *
+ */
+
+(function( $ ){
+
+	function ResourceBundle(name) {
+		var bundleName = name;
+		var bundle = {};
+		function build() {
+			var bundleRaw = $.trim($("script#" + bundleName ).text()).split("\n");
+			var bundleData = {};
+			for (var i=0; i<bundleRaw.length; i++) {
+				var row = $.trim(bundleRaw[i]);
+				var sep = row.indexOf("=");
+				bundleData[row.slice(0,sep)] = row.slice(sep+1);
+			}
+			bundle = bundleData;
+		}
+
+		build();
+		
+		return {
+			getString:function(key) {
+				return bundle[key];
+			}
+		};
+	}
+
+	$.extend({
+		getResourceBundle: function (name) {
+			return new ResourceBundle(name);		
+		}
+	});
+
+})( jQuery );
